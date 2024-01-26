86c4af41ed2b462b094fa73b192f3f6454187d2d
Константин Кизимов <kizim_net@mail.ru>
2024-01-26 23:48:37 +0400

    Create dockerfile

--- src/main/java/org/example/simplecontactlist/service/ContactServiceImpl.java
+++ src/main/java/org/example/simplecontactlist/service/ContactServiceImpl.java
@@ -1,7 +1,7 @@
 package org.example.simplecontactlist.service;
 
 import org.example.simplecontactlist.entitites.Contact;
-import org.example.simplecontactlist.repository.MyRepository;
+import org.example.simplecontactlist.repository.ContactRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,38 +9,40 @@ import java.util.List;
 
 @Service
 public class ContactServiceImpl implements ContactService{
-    private final MyRepository myRepository;
+    private final ContactRepository contactRepository;
 
-    public ContactServiceImpl(MyRepository myRepository) {
-        this.myRepository = myRepository;
+    public ContactServiceImpl(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
     }
+
+
     public List<Contact> selectAll(){
-        return new ArrayList<>(myRepository.selectAll());
+        return new ArrayList<>(contactRepository.selectAll());
     }
 
     @Override
     public Contact selectContact(long id) {
-        return (Contact) myRepository.selectById(id).orElse(null);
+        return contactRepository.selectById(id).orElse(null);
     }
 
     @Override
     public long addContact(Contact contact) {
         contact.setId(System.currentTimeMillis());
-        return myRepository.insert(contact);
+        return contactRepository.insert(contact);
     }
     @Override
     public void deleteContact(long id) {
-        myRepository.removeById(id);
+        contactRepository.removeById(id);
     }
 
     @Override
     public Contact updateContact(Contact contact) {
-        return (Contact) myRepository.update(contact);
+        return contactRepository.update(contact);
     }
 
     @Override
     public void addListContact(List<Contact> contactList) {
-        myRepository.batchInsert(contactList);
+        contactRepository.batchInsert(contactList);
     }
 
 
